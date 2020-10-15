49d947987da2f392240400f6e22c1bfcba7912df
Everton42 <evertonferreira4596@gmail.com>
2020-10-14 21:54:18 -0300

    projeto inicial

---
+++ src/main/java/com/cherrySpike/biometricAuth/BiometricAuthApplication.java
@@ -0,0 +1,13 @@
+package com.cherrySpike.biometricAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometricAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiometricAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherrySpike/biometricAuth/BiometricAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherrySpike.biometricAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
